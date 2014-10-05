36d1b5b1de66c53a36485d70250a20ade119737c
Theo Belaire <theo.belaire@gmail.com>
2014-10-05 14:30:47 -0400

    Now with space ships

---
+++ Assets/Scripts/ShipScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipScript : MonoBehaviour {
+
+	public float velocityCoefficient = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.rigidbody2D.velocity = velocityCoefficient *
+			new Vector2(Input.GetAxis("HorizontalShip1"),Input.GetAxis ("VerticalShip1"));
+	
+		if(Input.GetButtonDown("FireShip1")){
+			this.audio.Play ();
+			print ("Fireing");
+		}
+	}
+	
+}
